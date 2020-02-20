03a990838125d008530e0c10ceefa8c9c912bb19
Victor Dremov (Виктор Дремов) <37803199+dremovv@users.noreply.github.com>
2020-02-20 16:40:16 +0300

    update get_baseline_model description

--- src/baseline_model.py
+++ src/baseline_model.py
@@ -8,11 +8,9 @@ def get_baseline_model() -> implicit.als.AlternatingLeastSquares:
     environ["MKL_NUM_THREADS"] = "1"
     environ["OPENBLAS_NUM_THREADS"] = "1"
 
-    # to get these hyper parameters, we used random search
-    # in (factors, iterations, regularization) space
-    # then we took parameters that provide 80-percentile of map@10 score on test_purchases
-    # it means you can iterate through parameters and get better score
-
+    # we iterated through hyper parameters and measured map@10 score on test set
+    # the parameters below provide 80-th percentile of score
+    # we intentionally do not use parameters with best test score
     model = implicit.als.AlternatingLeastSquares(
         factors=20, iterations=7, regularization=100.0
     )
